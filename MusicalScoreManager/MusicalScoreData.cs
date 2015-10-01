845ef2a50b85d29f3ef917e818fa27bc465723c9
Happa8 <okadakoutaro@gmail.com>
2015-10-01 23:25:56 +0900

    楽譜情報用クラス作成

---
+++ MusicalScoreManager/MusicalScoreData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicalScoreManager
+{
+    class MusicalScoreData
+    {
+        public string ScoreName; //楽譜名
+        public string ScoreComposer; //作曲者名
+        public string ScoreArranger; //編曲者名
+
+    }
+}
