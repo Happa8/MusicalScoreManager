e3963f5e4c082838edc1f073c03f00027777ec66
Happa8 <okadakoutaro@gmail.com>
2015-10-02 22:46:00 +0900

    MSD作成クラス追加

---
+++ MusicalScoreManager/NewMSD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicalScoreManager
+{
+    class NewMSD
+    {
+        //public string MSDName = MSD_000001;
+
+        //MSD=MusicalScoreData
+
+        public void CreateMSD()
+        {
+            MusicalScoreData MSD_000001 = new MusicalScoreData();
+        }
+
+        
+        
+    }
+}
