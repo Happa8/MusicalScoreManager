17f8a15222c314013804fb6a4a27879ee4f32869
Happa8 <okadakoutaro@gmail.com>
2015-10-04 05:56:19 +0900

    新規作成ウィンドウ追加

--- MusicalScoreManager/NewMSDWindow.xaml.cs
+++ MusicalScoreManager/NewMSDWindow.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MusicalScoreManager
 {
     /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
+    /// Window1.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-            
-            
-        }
-
-        private void newMSD_Click(object sender, RoutedEventArgs e)
-        {
-            
         }
     }
 }
